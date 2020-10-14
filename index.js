85e7abd55795e05b227d862bdd94f9a0c100ebab
Your Name <you@example.com>
2020-10-15 07:00:48 +0800

    second commit

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const mongoose = require('mongoose')
 const port = process.env.PORT
 const mongodbCloud = "mongodb+srv://Lamji:Lamji492@cluster0.0rdew.mongodb.net/travel_tracker?retryWrites=true&w=majority"
 
-//add whitelisted origins here
-const corsOptions = {
-	origin: ['http://localhost:3000'],
-	optionsSuccessStatus: 200//for compatibility with older browsers
-}
+
+// const corsOptions = {
+// 	origin: ['http://localhost:3000'],
+// 	optionsSuccessStatus: 200
+// }
 
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB cloud.'))
 mongoose.connect(mongodbCloud, { 
@@ -24,8 +24,8 @@ mongoose.connect(mongodbCloud, {
 const app = express();
 const port = process.env.PORT || 3000;
 //bodyparser middleware
-app.use(express.json()) //only looks at request where the Content-Type header is JSON
-app.use(express.urlencoded({extended: true})) //allows POST requests to include nested objects
+// app.use(express.json()) //only looks at request where the Content-Type header is JSON
+// app.use(express.urlencoded({extended: true})) //allows POST requests to include nested objects
 
 //configure cors
 const corsOptions = {
